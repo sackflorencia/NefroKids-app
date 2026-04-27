6e0a68bbc726cda1ec93211befff98c28d45899b
Android Studio <devandroid@ort>
2026-04-27 12:22:44 -0300

    databaseistaller-faltacompletar

--- app/database/database.js
+++ app/database/database.js
@@ -1,11 +1,24 @@
 import * as SQLite from 'expo-sqlite';
-
+import { SQLiteProvider } from 'expo-sqlite';
 export const db = SQLite.openDatabase('miDB.db');
 
-export const initDB = () => {
-  db.transaction(tx => {
-    tx.executeSql(
-      ``
+export default function initDB () { 
+  return(
+  <SQLiteProvider
+  databasename="NefroKids.db"
+  onInit={async (db) => {
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS users
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      firstName TEXT NOT NULL,
+      lastName TEXT NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      phone TEXT NOT NULL,
     );
-  });
-};
+    PRAGMA journal_mode=WAL;
+    `);
+  }}
+  options={{useNewConnection: false }}
+>
+  );
+}
