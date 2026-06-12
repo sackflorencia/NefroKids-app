f0696892720f96b7295b46621e34dfe790a8f758
Flor <florenciasack123@gmail.com>
2026-06-12 10:43:17 -0300

    begining firebase

--- back/services/SyncService.js
+++ back/services/SyncService.js
@@ -1,9 +1,69 @@
-class SyncService {
+import FirebaseService from "./FirebaseService";
 
-  async uploadChildData() {}
+import {
+  doc,
+  setDoc,
+  serverTimestamp
+} from "firebase/firestore";
 
-  async downloadChildData() {}
+export default class SyncService {
 
-  async backupAll() {}
+  constructor() {
+    this.db = FirebaseService.getFirestore();
+  }
+
+
+  async createChild(child) {
+
+    await setDoc(
+      doc(this.db, "children", child.id),
+      {
+        full_name: child.full_name,
+
+        birth_date: child.birth_date,
+
+        first_register_date: new Date(child.first_register_date),
+
+        total_xp: child.total_xp,
+
+        urinates: Boolean(child.urinates),
+
+        avatar_id: child.avatar_id,
+
+        tutor_uids: [],
+
+        created_at: serverTimestamp(),
+
+        updated_at: serverTimestamp()
+      }
+    );
+
+  }
+  async createTutor(uid, tutor) {
+
+    await setDoc(
+      doc(this.db, "tutors", uid),
+      {
+        child_id: tutor.child_id,
+
+        full_name: tutor.full_name,
+
+        email: tutor.email,
+
+        phone: tutor.phone,
+
+        relationship: tutor.relationship,
+
+        is_primary:
+          Boolean(tutor.is_primary),
+
+        created_at:
+          serverTimestamp(),
+
+        updated_at:
+          serverTimestamp()
+      }
+    );
+  }
 
 }
