f697e06ac863ecc5e1d1a1561ab37c5c892b9304
Flor <florenciasack123@gmail.com>
2026-07-08 14:43:29 -0300

    Created a UserContext
    implemented it in guardian registration and login flow

--- App.js
+++ App.js
@@ -3,14 +3,19 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import InitDB from "./back/database";
 import "react-native-get-random-values";
 import FirebaseService from "./back/services/FirebaseService";
+import { UserProvider } from "./app/context/UserContext";
 
 FirebaseService.initialize();
 
 export default function App() {
 
   return (
-    <InitDB>
-      <AppNavigator />
-    </InitDB>
+    <>
+      <UserProvider>
+        <InitDB>
+          <AppNavigator />
+        </InitDB>
+      </UserProvider>
+    </>
   );
 }
